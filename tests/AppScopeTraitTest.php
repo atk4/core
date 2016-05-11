a84c61171131c70c1f9d0ac103476a6f9278506d
Romans Malinovskis <romans@agiletoolkit.org>
2016-05-11 15:36:29 +0100

    fix test suite

---
+++ tests/AppScopeTraitTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace atk4\core\tests;
+
+use atk4\core\AppScopeTrait;
+
+/**
+ * @coversDefaultClass \atk4\data\Model
+ */
+class AppScopeTraitTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Test constructor
+     *
+     */
+    public function testConstruct()
+    {
+        $m = new AppScopeMock();
+        $m->app="myapp";
+    }
+
+}
+
+class AppScopeMock {
+}
