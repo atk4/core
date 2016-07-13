cdc436a6a778c667b61ecd7971db1cd7748a527d
Romans Malinovskis <romans@agiletoolkit.org>
2016-07-13 14:09:34 +0100

    alphabetize trait list

--- tests/AppScopeTraitTest.php
+++ tests/AppScopeTraitTest.php
@@ -4,8 +4,8 @@ namespace atk4\core\tests;
 
 use atk4\core\AppScopeTrait;
 use atk4\core\ContainerTrait;
-use atk4\core\TrackableTrait;
 use atk4\core\NameTrait;
+use atk4\core\TrackableTrait;
 
 /**
  * @coversDefaultClass \atk4\data\Model
