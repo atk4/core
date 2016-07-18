637e2c26f025c23f421619d6dc9ad62e876d0c90
Imants Horsts <imants.horsts@inbox.lv>
2016-07-18 18:18:41 +0300

    * simplify session trait and get rid of explicit serialization * add tests * add docs

--- tests/SessionTraitTest.php
+++ tests/SessionTraitTest.php
@@ -3,6 +3,7 @@
 namespace atk4\core\tests;
 
 use atk4\core\SessionTrait;
+use atk4\core\NameTrait;
 
 /**
  * @coversDefaultClass \atk4\core\SessionTrait
@@ -10,24 +11,99 @@ use atk4\core\SessionTrait;
 class SessionTraitTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @expectedException     Exception
+     */
+    public function testException1()
+    {
+        // when try to start session without NameTrait
+        $m = new SessionWithoutNameMock();
+        $m->startSession();
+    }
+
+    /**
      * Test constructor.
      */
     public function testConstructor()
     {
         $m = new SessionMock();
 
-        // not implemented yet
-        $this->assertEquals(null, $m->memorize());
-        $this->assertEquals(null, $m->learn());
-        $this->assertEquals(null, $m->recall());
-        $this->assertEquals(null, $m->forget());
+        $this->assertEquals(false, isset($_SESSION));
+        $m->startSession();
+        $this->assertEquals(true, isset($_SESSION));
+        $m->destroySession();
+        $this->assertEquals(false, isset($_SESSION));
+    }
 
-        $this->markTestIncomplete();
+    /**
+     * Test memorize().
+     */
+    public function testMemorize()
+    {
+        $m = new SessionMock();
+        $m->name = 'test';
+
+        // value as string
+        $m->memorize('foo', 'bar');
+        $this->assertEquals('bar', $_SESSION['o'][$m->name]['foo']);
+        
+        // value as null
+        $m->memorize('foo', null);
+        $this->assertEquals(null, $_SESSION['o'][$m->name]['foo']);
+
+        // value as callable
+        $m->memorize('foo', function () {
+            return 'bar';
+        });
+        $this->assertEquals('bar', $_SESSION['o'][$m->name]['foo']);
+
+        // value as object
+        $o = new \StdClass;
+        $m->memorize('foo', $o);
+        $this->assertEquals($o, $_SESSION['s'][$m->name]['foo']);
+
+        $m->destroySession();
+    }
+
+    /**
+     * Test learn(), recall(), forget().
+     */
+    public function testLearnRecallForget()
+    {
+        $m = new SessionMock();
+        $m->name = 'test';
+
+        // value as string
+        $m->learn('foo', 'bar');
+        $this->assertEquals('bar', $m->recall('foo'));
+
+        $m->learn('foo', 'qwerty');
+        $this->assertEquals('bar', $m->recall('foo'));
+
+        $m->forget('foo');
+        $this->assertEquals('undefined', $m->recall('foo', 'undefined'));
+
+        // value as callback
+        $m->learn('foo', function () {
+            return 'bar';
+        });
+        $this->assertEquals('bar', $m->recall('foo'));
+
+        $m->learn('foo_2', 'another');
+        $this->assertEquals('another', $m->recall('foo_2'));
+
+        $m->forget();
+        $this->assertEquals('undefined', $m->recall('foo', 'undefined'));
+        $this->assertEquals('undefined', $m->recall('foo_2', 'undefined'));
     }
 }
 
 // @codingStandardsIgnoreStart
 class SessionMock
+{
+    use SessionTrait;
+    use NameTrait;
+}
+class SessionWithoutNameMock
 {
     use SessionTrait;
 }
