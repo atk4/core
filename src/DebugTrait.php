2cc086899f5d7ee69ebf3d13735646e052bf3630
Romans Malinovskis <romans@agiletoolkit.org>
2016-07-25 16:49:37 +0100

    fix style.

--- src/DebugTrait.php
+++ src/DebugTrait.php
@@ -19,11 +19,11 @@ trait DebugTrait
             return $this;
         }
 
-        return (isset($this->app) && isset($this->app->debug) && $this->app->debug);
+        return isset($this->app) && isset($this->app->debug) && $this->app->debug;
     }
 
     /**
-     * Send some info to debug stream
+     * Send some info to debug stream.
      *
      * @param bool $enable
      *
@@ -41,7 +41,7 @@ trait DebugTrait
 
             if (
                 (isset($this->app->_dynamicMethodTrait) && $this->app->hasMethod('outputDebug')) ||
-                method_exists($this->app, 'outputDebug') 
+                method_exists($this->app, 'outputDebug')
             ) {
                 $this->app->outputDebug($msg, $extra_info);
             } else {
@@ -59,17 +59,19 @@ trait DebugTrait
     {
         if ($this->isDebugEnabled()) {
             $bt = [];
-            foreach (debug_backtrace() as $line) if (isset($line['file'])) {
-                $bt[] = $line['file'].':'.$line['line'];
+            foreach (debug_backtrace() as $line) {
+                if (isset($line['file'])) {
+                    $bt[] = $line['file'].':'.$line['line'];
+                }
             }
 
 
-            if(isset($this->_prev_bt[$trace]) && array_diff($this->_prev_bt[$trace], $bt)) {
+            if (isset($this->_prev_bt[$trace]) && array_diff($this->_prev_bt[$trace], $bt)) {
 
                 $d1 = array_diff($this->_prev_bt[$trace], $bt);
                 $d2 = array_diff($bt, $this->_prev_bt[$trace]);
 
-                $this->debug("Call path for ".$trace." has diverged (was ".implode(', ',$d1).", now ".implode(', ',$d2).")\n");
+                $this->debug("Call path for ".$trace." has diverged (was ".implode(', ', $d1).", now ".implode(', ', $d2).")\n");
             }
 
 
