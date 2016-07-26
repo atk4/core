01213982978cc1f5152459a8aeb5fa3188bc2fce
Romans Malinovskis <me@nearly.guru>
2016-07-26 16:08:38 +0100

    Feature/implement debug (#25)
    * Implement Debug Trait
    
    * Document
    
    * fix style.
    
    * style.
    
    * fix style.
    
    * fix style.
    
    * improve tracking of $app.
    
    * fix test.

--- src/DebugTrait.php
+++ src/DebugTrait.php
@@ -11,19 +11,69 @@ trait DebugTrait
      */
     public $_debugTrait = true;
 
-    public $debug = false;
+    public $debug = null;
+
+    protected function isDebugEnabled()
+    {
+        if ($this->debug === false || $this->debug === true) {
+            return $this;
+        }
+
+        return isset($this->app) && isset($this->app->debug) && $this->app->debug;
+    }
 
     /**
-     * Switch on/off debug state.
+     * Send some info to debug stream.
      *
      * @param bool $enable
      *
      * @return $this
      */
-    public function debug($enable = true)
+    public function debug($msg = true, $extra_info = [])
     {
-        $this->debug = (bool) $enable;
+        if (is_bool($msg)) {
+            // using this to switch on/off the debug for this object
+            $this->debug = $msg;
+
+            return $this;
+        }
+
+        if ($this->isDebugEnabled()) {
+            if (
+                isset($this->app) && ((isset($this->app->_dynamicMethodTrait) && $this->app->hasMethod('outputDebug')) ||
+                method_exists($this->app, 'outputDebug'))
+            ) {
+                $this->app->outputDebug($msg, $extra_info);
+            } else {
+                fwrite(STDERR, '['.get_class($this)."]: $msg\n");
+            }
+        }
 
         return $this;
+    }
+
+    public $_prev_bt = [];
+
+    public function debugTraceChange($trace = 'default')
+    {
+        if ($this->isDebugEnabled()) {
+            $bt = [];
+            foreach (debug_backtrace() as $line) {
+                if (isset($line['file'])) {
+                    $bt[] = $line['file'].':'.$line['line'];
+                }
+            }
+
+
+            if (isset($this->_prev_bt[$trace]) && array_diff($this->_prev_bt[$trace], $bt)) {
+                $d1 = array_diff($this->_prev_bt[$trace], $bt);
+                $d2 = array_diff($bt, $this->_prev_bt[$trace]);
+
+                $this->debug('Call path for '.$trace.' has diverged (was '.implode(', ', $d1).', now '.implode(', ', $d2).")\n");
+            }
+
+
+            $this->_prev_bt[$trace] = $bt;
+        }
     }
 }
