cc400046068e5b0c3385d7c58542407b490bdc28
Romans Malinovskis <romans@agiletoolkit.org>
2017-09-06 13:34:18 +0100

    improve coverage

--- tests/ExceptionTest.php
+++ tests/ExceptionTest.php
@@ -21,7 +21,6 @@ class ExceptionTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(['a1' => 111, 'a2' => 222], $m->getParams());
 
         $m = new Exception('PrevError');
-
         $m = new Exception('TestIt', 123, $m);
         $m->addMoreInfo('a1', 222);
         $m->addMoreInfo('a2', 333);
@@ -56,6 +55,25 @@ class ExceptionTest extends \PHPUnit_Framework_TestCase
         $ret = $m->toString($a);
         $this->assertEquals('atk4\core\tests\TrackableMock2 (foo)', $ret);
     }
+
+    public function testMore()
+    {
+        $m = new \Exception('Classic Exception');
+
+
+        $m = new Exception('atk4 exception', null, $m);
+        $m->setMessage('bumbum');
+
+        $ret = $m->getColorfulText();
+        $this->assertRegExp('/Classic/', $ret);
+        $this->assertRegExp('/bumbum/', $ret);
+
+        // get colorful text
+        $ret = $m->getHTML();
+        $this->assertRegExp('/Classic/', $ret);
+        $this->assertRegExp('/bumbum/', $ret);
+    }
+
 }
 
 // @codingStandardsIgnoreStart
