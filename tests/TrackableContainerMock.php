59e921d21528e8aff3e9cd1ce11237be8f551c63
Romans Malinovskis <romans@agiletoolkit.org>
2017-09-12 17:10:21 +0100

    tests combination of trackable/container/factory

---
+++ tests/TrackableContainerMock.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace atk4\core\tests;
+
+use atk4\core;
+
+class TrackableContainerMock
+{
+    use core\NameTrait;
+    use core\ContainerTrait;
+    use core\TrackableTrait;
+
+    public function getElementCount()
+    {
+        return count($this->elements);
+    }
+}
