92bb8b85cd2990ebc7c7ccdffb3b5008842dd60c
abbadon1334 <fdanti@gmail.com>
2019-06-16 13:10:21 +0300

    Simplify TranslatorInterface - singular is always = plural 1 of the string

--- src/TranslatableTrait.php
+++ src/TranslatableTrait.php
@@ -22,33 +22,30 @@ trait TranslatableTrait
      *
      * @return string
      */
-    public function _(string $message, int $count = 1)
+    public function _(string $message, int $count = 1): string
     {
         if (isset($this->app) && method_exists($this->app, '_')) {
-            return $this->app->_($message, $count ?? 1);
+            return $this->app->_($message, $count, 'atk4');
         }
 
         return $message;
     }
 
     /**
-     * Get the translation of a message in the correct plural form
+     * Get the translation of a message in the correct plural form with contaxt domain
      *
-     * @param string   $message the text to translate
-     * @param string   $domain  the context domain if exists
-     * @param int|null $count   the counter used to evaluate the plural
+     * @param string $message the text to translate
+     * @param string $context the context domain if exists
+     * @param int    $count   the counter used to evaluate the plural
      *
      * @return string
      */
-    public function _d(string $message, string $domain = 'atk4', int $count = 1)
+    public function _d(string $message, string $context = 'atk4', int $count = 1): string
     {
-        if (isset($this->app) && method_exists($this->app, '_d')) {
-            return $this->app->_d($message, $domain, $count);
+        if (isset($this->app) && method_exists($this->app, '_')) {
+            return $this->app->_($message, $count, $context);
         }
 
         return $message;
     }
 }
-
-//
-// $this->_m('test %s %s', null, ['test','testing',['test',null,['test']]])
--- src/TranslatorInterface.php
+++ src/TranslatorInterface.php
@@ -5,23 +5,13 @@ namespace atk4\core;
 interface TranslatorInterface
 {
     /**
-     * Get the translation of a message
-     *
-     * @param  string      $message The text to translate
-     * @param  string|null $context The context if exists
-     *
-     * @return string
-     */
-    public function translate(string $message, ?string $context) :string;
-
-    /**
      * Get the translation of a message in the correct plural form
      *
-     * @param  integer     $count   The counter used to evaluate the plural
-     * @param  string      $message The text to translate (in singular)
-     * @param  string|null $context The context if exists
+     * @param string      $message The text to translate (always singular)
+     * @param integer     $count   The counter used to evaluate the plural
+     * @param string|null $context The context if exists
      *
      * @return string
      */
-    public function translatePlural(int $count, string $message, ?string $context) :string;
+    public function translate(string $message, int $count = 1, ?string $context = NULL): string;
 }
