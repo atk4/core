8025d2b63ea2fcbf46ba7d0cc198231ca52f39ae
abbadon1334 <fdanti@gmail.com>
2019-06-22 07:34:58 +0300

    TranslatableTrait + Test using Symfony TranslationInterface

--- src/TranslatableTrait.php
+++ src/TranslatableTrait.php
@@ -2,6 +2,9 @@
 
 namespace atk4\core;
 
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Contracts\Translation\TranslatorTrait;
+
 /**
  * If a class use this trait, string can be translated calling method translate.
  */
@@ -15,20 +18,86 @@ trait TranslatableTrait
     public $_translatableTrait = true;
 
     /**
-     * Get the translation of a message in the correct plural form.
-     *
-     * @param string      $message
-     * @param array|null  $args
-     * @param string|null $context
-     *
-     * @return string
+     * @var TranslatorInterface
      */
-    public function _(string $message,?array $args=null,?string $context=null)
+    protected $translator;
+
+    public function setTranslator(TranslatorInterface $translator)
     {
-        if (isset($this->app) && method_exists($this->app, '_')) {
-            return $this->app->_($message,$args,$context);
+        $this->translator = $translator;
+    }
+
+    public function getTranslator() : ?TranslatorInterface
+    {
+        return $this->translator;
+    }
+
+    /**
+     * Translates the given message.
+     *
+     * When a number is provided as a parameter named "%count%", the message is parsed for plural
+     * forms and a translation is chosen according to this number using the following rules:
+     *
+     * Given a message with different plural translations separated by a
+     * pipe (|), this method returns the correct portion of the message based
+     * on the given number, locale and the pluralization rules in the message
+     * itself.
+     *
+     * The message supports two different types of pluralization rules:
+     *
+     * interval: {0} There are no apples|{1} There is one apple|]1,Inf] There are %count% apples
+     * indexed:  There is one apple|There are %count% apples
+     *
+     * The indexed solution can also contain labels (e.g. one: There is one apple).
+     * This is purely for making the translations more clear - it does not
+     * affect the functionality.
+     *
+     * The two methods can also be mixed:
+     *     {0} There are no apples|one: There is one apple|more: There are %count% apples
+     *
+     * An interval can represent a finite set of numbers:
+     *  {1,2,3,4}
+     *
+     * An interval can represent numbers between two numbers:
+     *  [1, +Inf]
+     *  ]-1,2[
+     *
+     * The left delimiter can be [ (inclusive) or ] (exclusive).
+     * The right delimiter can be [ (exclusive) or ] (inclusive).
+     * Beside numbers, you can use -Inf and +Inf for the infinite.
+     *
+     * @see https://en.wikipedia.org/wiki/ISO_31-11
+     *
+     * @param string      $id         The message id (may also be an object that can be cast to string)
+     * @param array       $parameters An array of parameters for the message
+     * @param string|null $domain     The domain for the message or null to use the default
+     * @param string|null $locale     The locale or null to use the default
+     *
+     * @return string The translated string
+     *
+     * @throws \InvalidArgumentException If the locale contains invalid characters
+     */
+    public function _($id, array $parameters = [], $domain = null, $locale = null) :string
+    {
+        // !!! Check for translator must be first to avoid infinite loop on when $this === $this->app
+        // check if there is a translator which implements Symfony TranslatorInterface
+        // case "we are NOT in atk4/ui"
+        if (isset($this->translator) && $this->translator instanceof TranslatorInterface) {
+            return $this->translator->trans($id, $parameters, $domain, $locale);
         }
 
-        return $message;
+        // check if there is method _ in $this->app
+        // case "we are in atk4/ui" AppScope + Container
+        if (isset($this->app) && method_exists($this->app, '_') && $this->app !== $this) {
+            return $this->app->_($id, $parameters, $domain, $locale);
+        }
+
+        // if translator is not present use the the trait from Symfony
+        // to replicate internal library functionality
+        $translator = new class() implements TranslatorInterface {
+            use TranslatorTrait;
+        };
+
+        return $translator->trans($id, $parameters, $domain, $locale);
     }
 }
--- tests/TranslatableTraitTest.php
+++ tests/TranslatableTraitTest.php
@@ -3,46 +3,326 @@
 namespace atk4\core\tests;
 
 use atk4\core\AppScopeTrait;
+use atk4\core\ContainerTrait;
 use atk4\core\TranslatableTrait;
 use atk4\core\Translator;
 use PHPUnit\Framework\TestCase;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Contracts\Translation\TranslatorTrait;
 
 class TranslatableTraitTest extends TestCase
 {
-    /** @var TranslatableTraitMock */
-    public $mock;
-
-    public function setUp() :void
+    private function getTranslatorMock()
     {
-        $this->mock = new TranslatableTraitMock();
+        return new class() implements TranslatorInterface {
+            use TranslatorTrait;
+        };
     }
 
-    public function dataProvider()
+    private function getMockCaseApp($translator = null)
     {
-        return [['string to test','string to test']];
+        $app = new class($translator) {
+
+            use AppScopeTrait;
+            use ContainerTrait;
+            use TranslatableTrait;
+
+            public function __construct($translator)
+            {
+                $this->app = $this;
+
+                if(null !== $translator)
+                    $this->setTranslator($translator);
+            }
+        };
+
+        return $app;
+    }
+
+    private function getMockCaseAppChild($translator=null)
+    {
+        $app = $this->getMockCaseApp($translator);
+
+        $child = new class() {
+            use AppScopeTrait;
+            use TranslatableTrait;
+        };
+
+        $app->add($child);
+
+        return $child;
+    }
+
+    private function getMockCaseNoATK($translator = null)
+    {
+        return new class($translator) {
+
+            use TranslatableTrait;
+
+            public function __construct($translator)
+            {
+                if(null !== $translator)
+                    $this->setTranslator($translator);
+            }
+        };
     }
 
     /**
-     * @dataProvider dataProvider
-     *
-     * @param $input    input string to test
-     * @param $output   excepted output
+     * @dataProvider getTransTests
      */
-    public function testMethod_($input, $output)
+    public function testSetTranslator()
     {
-        $trans = $this->mock->_($input);
-        $this->assertEquals($output, $trans);
+        $mock = new class() {
 
-        $trans = $this->mock->_($input,[]);
-        $this->assertEquals($output, $trans);
+            use TranslatableTrait;
+        };
 
-        $trans = $this->mock->_($input,[],$input);
-        $this->assertEquals($output, $trans);
+        $mock->setTranslator($this->getTranslatorMock());
+        $this->assertTrue(is_a($mock->getTranslator(),TranslatorInterface::class));
+    }
+
+    /**
+     * @dataProvider getTransTests
+     *
+     * @param $expected
+     * @param $id
+     * @param $parameters
+     */
+    public function testTrans($expected, $id, $parameters)
+    {
+        $app = $this->getMockCaseApp();
+        $this->assertEquals($expected, $app->_($id, $parameters));
+
+        $app = $this->getMockCaseAppChild();
+        $this->assertEquals($expected, $app->_($id, $parameters));
+
+        $app = $this->getMockCaseNoATK();
+        $this->assertEquals($expected, $app->_($id, $parameters));
+
+        // with Translator
+
+        $app = $this->getMockCaseApp($this->getTranslatorMock());
+        $this->assertEquals($expected, $app->_($id, $parameters));
+
+        $app = $this->getMockCaseAppChild($this->getTranslatorMock());
+        $this->assertEquals($expected, $app->_($id, $parameters));
+
+        $app = $this->getMockCaseNoATK($this->getTranslatorMock());
+        $this->assertEquals($expected, $app->_($id, $parameters));
+    }
+
+    /**
+     * @dataProvider getChooseTests
+     *
+     * @param $expected
+     * @param $id
+     * @param $number
+     */
+    public function testChoose($expected, $id, $number)
+    {
+        $app = $this->getMockCaseApp();
+        $this->assertEquals($expected, $app->_($id, ['%count%' => $number]));
+
+        $app = $this->getMockCaseAppChild();
+        $this->assertEquals($expected, $app->_($id, ['%count%' => $number]));
+
+        $app = $this->getMockCaseNoATK();
+        $this->assertEquals($expected, $app->_($id, ['%count%' => $number]));
+
+        // with Translator
+
+        $app = $this->getMockCaseApp($this->getTranslatorMock());
+        $this->assertEquals($expected, $app->_($id, ['%count%' => $number]));
+
+        $app = $this->getMockCaseAppChild($this->getTranslatorMock());
+        $this->assertEquals($expected, $app->_($id, ['%count%' => $number]));
+
+        $app = $this->getMockCaseNoATK($this->getTranslatorMock());
+        $this->assertEquals($expected, $app->_($id, ['%count%' => $number]));
+
+    }
+
+    /**
+     * @dataProvider getTransChoiceTests
+     *
+     * @param $expected
+     * @param $id
+     * @param $number
+     */
+    public function testTransChoice($expected, $id, $number)
+    {
+        $app = $this->getMockCaseApp();
+        $this->assertEquals($expected, $app->_($id, ['%count%' => $number]));
+
+        $app = $this->getMockCaseAppChild();
+        $this->assertEquals($expected, $app->_($id, ['%count%' => $number]));
+
+        $app = $this->getMockCaseNoATK();
+        $this->assertEquals($expected, $app->_($id, ['%count%' => $number]));
+
+        // with Translator
+
+        $app = $this->getMockCaseApp($this->getTranslatorMock());
+        $this->assertEquals($expected, $app->_($id, ['%count%' => $number]));
+
+        $app = $this->getMockCaseAppChild($this->getTranslatorMock());
+        $this->assertEquals($expected, $app->_($id, ['%count%' => $number]));
+
+        $app = $this->getMockCaseNoATK($this->getTranslatorMock());
+        $this->assertEquals($expected, $app->_($id, ['%count%' => $number]));
+    }
+
+    /**
+     * @dataProvider getNonMatchingMessages
+     *
+     * @param $id
+     * @param $number
+     */
+    public function testThrowExceptionIfMatchingMessageCannotBeFound1($id, $number)
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $app = $this->getMockCaseApp();
+        $this->assertEquals($id, $app->_($id, ['%count%' => $number]));
+    }
+
+    /**
+     * @dataProvider getNonMatchingMessages
+     *
+     * @param $id
+     * @param $number
+     */
+    public function testThrowExceptionIfMatchingMessageCannotBeFound2($id, $number)
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $app = $this->getMockCaseAppChild();
+        $this->assertEquals($id, $app->_($id, ['%count%' => $number]));
+    }
+
+    /**
+     * @dataProvider getNonMatchingMessages
+     *
+     * @param $id
+     * @param $number
+     */
+    public function testThrowExceptionIfMatchingMessageCannotBeFound3($id, $number)
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $app = $this->getMockCaseNoATK();
+        $this->assertEquals($id, $app->_($id, ['%count%' => $number]));
+    }
+
+    // Data Providers FROM Symfony Translator Test
+
+    public function getTransTests()
+    {
+        return [
+            ['Symfony is great!', 'Symfony is great!', []],
+            ['Symfony is awesome!', 'Symfony is %what%!', ['%what%' => 'awesome']],
+        ];
+    }
+
+    public function getTransChoiceTests()
+    {
+        return [
+            ['There are no apples', '{0} There are no apples|{1} There is one apple|]1,Inf] There are %count% apples', 0],
+            ['There is one apple', '{0} There are no apples|{1} There is one apple|]1,Inf] There are %count% apples', 1],
+            ['There are 10 apples', '{0} There are no apples|{1} There is one apple|]1,Inf] There are %count% apples', 10],
+            ['There are 0 apples', 'There is 1 apple|There are %count% apples', 0],
+            ['There is 1 apple', 'There is 1 apple|There are %count% apples', 1],
+            ['There are 10 apples', 'There is 1 apple|There are %count% apples', 10],
+            // custom validation messages may be coded with a fixed value
+            ['There are 2 apples', 'There are 2 apples', 2],
+        ];
+    }
+
+    public function getChooseTests()
+    {
+        return [
+            ['There are no apples', '{0} There are no apples|{1} There is one apple|]1,Inf] There are %count% apples', 0],
+            ['There are no apples', '{0}     There are no apples|{1} There is one apple|]1,Inf] There are %count% apples', 0],
+            ['There are no apples', '{0}There are no apples|{1} There is one apple|]1,Inf] There are %count% apples', 0],
+
+            ['There is one apple', '{0} There are no apples|{1} There is one apple|]1,Inf] There are %count% apples', 1],
+
+            ['There are 10 apples', '{0} There are no apples|{1} There is one apple|]1,Inf] There are %count% apples', 10],
+            ['There are 10 apples', '{0} There are no apples|{1} There is one apple|]1,Inf]There are %count% apples', 10],
+            ['There are 10 apples', '{0} There are no apples|{1} There is one apple|]1,Inf]     There are %count% apples', 10],
+
+            ['There are 0 apples', 'There is one apple|There are %count% apples', 0],
+            ['There is one apple', 'There is one apple|There are %count% apples', 1],
+            ['There are 10 apples', 'There is one apple|There are %count% apples', 10],
+
+            ['There are 0 apples', 'one: There is one apple|more: There are %count% apples', 0],
+            ['There is one apple', 'one: There is one apple|more: There are %count% apples', 1],
+            ['There are 10 apples', 'one: There is one apple|more: There are %count% apples', 10],
+
+            ['There are no apples', '{0} There are no apples|one: There is one apple|more: There are %count% apples', 0],
+            ['There is one apple', '{0} There are no apples|one: There is one apple|more: There are %count% apples', 1],
+            ['There are 10 apples', '{0} There are no apples|one: There is one apple|more: There are %count% apples', 10],
+
+            ['', '{0}|{1} There is one apple|]1,Inf] There are %count% apples', 0],
+            ['', '{0} There are no apples|{1}|]1,Inf] There are %count% apples', 1],
+
+            // Indexed only tests which are Gettext PoFile* compatible strings.
+            ['There are 0 apples', 'There is one apple|There are %count% apples', 0],
+            ['There is one apple', 'There is one apple|There are %count% apples', 1],
+            ['There are 2 apples', 'There is one apple|There are %count% apples', 2],
+
+            // Tests for float numbers
+            ['There is almost one apple', '{0} There are no apples|]0,1[ There is almost one apple|{1} There is one apple|[1,Inf] There is more than one apple', 0.7],
+            ['There is one apple', '{0} There are no apples|]0,1[There are %count% apples|{1} There is one apple|[1,Inf] There is more than one apple', 1],
+            ['There is more than one apple', '{0} There are no apples|]0,1[There are %count% apples|{1} There is one apple|[1,Inf] There is more than one apple', 1.7],
+            ['There are no apples', '{0} There are no apples|]0,1[There are %count% apples|{1} There is one apple|[1,Inf] There is more than one apple', 0],
+            ['There are no apples', '{0} There are no apples|]0,1[There are %count% apples|{1} There is one apple|[1,Inf] There is more than one apple', 0.0],
+            ['There are no apples', '{0.0} There are no apples|]0,1[There are %count% apples|{1} There is one apple|[1,Inf] There is more than one apple', 0],
+
+            // Test texts with new-lines
+            // with double-quotes and \n in id & double-quotes and actual newlines in text
+            ["This is a text with a\n            new-line in it. Selector = 0.", '{0}This is a text with a
+            new-line in it. Selector = 0.|{1}This is a text with a
+            new-line in it. Selector = 1.|[1,Inf]This is a text with a
+            new-line in it. Selector > 1.', 0],
+            // with double-quotes and \n in id and single-quotes and actual newlines in text
+            ["This is a text with a\n            new-line in it. Selector = 1.", '{0}This is a text with a
+            new-line in it. Selector = 0.|{1}This is a text with a
+            new-line in it. Selector = 1.|[1,Inf]This is a text with a
+            new-line in it. Selector > 1.', 1],
+            ["This is a text with a\n            new-line in it. Selector > 1.", '{0}This is a text with a
+            new-line in it. Selector = 0.|{1}This is a text with a
+            new-line in it. Selector = 1.|[1,Inf]This is a text with a
+            new-line in it. Selector > 1.', 5],
+            // with double-quotes and id split accros lines
+            ['This is a text with a
+            new-line in it. Selector = 1.', '{0}This is a text with a
+            new-line in it. Selector = 0.|{1}This is a text with a
+            new-line in it. Selector = 1.|[1,Inf]This is a text with a
+            new-line in it. Selector > 1.', 1],
+            // with single-quotes and id split accros lines
+            ['This is a text with a
+            new-line in it. Selector > 1.', '{0}This is a text with a
+            new-line in it. Selector = 0.|{1}This is a text with a
+            new-line in it. Selector = 1.|[1,Inf]This is a text with a
+            new-line in it. Selector > 1.', 5],
+            // with single-quotes and \n in text
+            ['This is a text with a\nnew-line in it. Selector = 0.', '{0}This is a text with a\nnew-line in it. Selector = 0.|{1}This is a text with a\nnew-line in it. Selector = 1.|[1,Inf]This is a text with a\nnew-line in it. Selector > 1.', 0],
+            // with double-quotes and id split accros lines
+            ["This is a text with a\nnew-line in it. Selector = 1.", "{0}This is a text with a\nnew-line in it. Selector = 0.|{1}This is a text with a\nnew-line in it. Selector = 1.|[1,Inf]This is a text with a\nnew-line in it. Selector > 1.", 1],
+            // esacape pipe
+            ['This is a text with | in it. Selector = 0.', '{0}This is a text with || in it. Selector = 0.|{1}This is a text with || in it. Selector = 1.', 0],
+            // Empty plural set (2 plural forms) from a .PO file
+            ['', '|', 1],
+            // Empty plural set (3 plural forms) from a .PO file
+            ['', '||', 1],
+        ];
+    }
+
+    public function getNonMatchingMessages()
+    {
+        return [
+            ['{0} There are no apples|{1} There is one apple', 2],
+            ['{1} There is one apple|]1,Inf] There are %count% apples', 0],
+            ['{1} There is one apple|]2,Inf] There are %count% apples', 2],
+            ['{0} There are no apples|There is one apple', 2],
+        ];
     }
 }
-
-// @codingStandardsIgnoreStart
-class TranslatableTraitMock {
-    use TranslatableTrait;
-}
-// @codingStandardsIgnoreEnd
