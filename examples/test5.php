cfd804feb96d4501060a47db113e5c02cce10858
abbadon1334 <fdanti@gmail.com>
2019-09-27 17:48:45 +0300

    add Example Test for JSON

---
+++ examples/test5.php
@@ -0,0 +1,28 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use atk4\core\Exception;
+
+function faulty($test)
+{
+    if ($test > 5) {
+
+        $exception_prev = new \Exception('Previous Exception');
+
+        $exception = new Exception([
+            'Test value is too high',
+            'test' => $test,
+        ],200, $exception_prev);
+        $exception->addSolution('Suggested solution test');
+        throw $exception;
+    }
+
+    return faulty($test + 1);
+}
+
+try {
+    faulty(1);
+} catch (Exception $e) {
+    echo $e->getJSON();
+}
