a5e934abdbea1b676dfc8e42131d3baa952e338b
abbadon1334 <fdanti@gmail.com>
2019-09-27 17:23:00 +0300

    Improve Test and Examples

--- examples/test4.php
+++ examples/test4.php
@@ -7,10 +7,15 @@ use atk4\core\Exception;
 function faulty($test)
 {
     if ($test > 5) {
-        throw new Exception([
+
+        $exception_prev = new \Exception('Previous Exception');
+
+        $exception = new Exception([
             'Test value is too high',
             'test' => $test,
-        ]);
+        ],200, $exception_prev);
+        $exception->addSolution('Suggested solution test');
+        throw $exception;
     }
 
     return faulty($test + 1);
