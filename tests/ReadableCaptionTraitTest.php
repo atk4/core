c6d5d65a7425ba6f6b712500c0439face97095f5
Imants Horsts <imants.horsts@inbox.lv>
2019-11-26 15:03:14 +0000

    Apply fixes from StyleCI

--- tests/ReadableCaptionTraitTest.php
+++ tests/ReadableCaptionTraitTest.php
@@ -15,7 +15,7 @@ class ReadableCaptionTraitTest extends \atk4\core\PHPUnit7_AgileTestCase
     public function testReadableCaption()
     {
         $a = new ReadableCaptionMock();
-        
+
         $this->assertEquals('User Defined Entity', $a->readableCaption('userDefinedEntity'));
         $this->assertEquals('New NASA Module', $a->readableCaption('newNASA_module'));
         $this->assertEquals('This Is NASA My Big Bull Shit 123 Foo', $a->readableCaption('this\\ _isNASA_MyBigBull shit_123\Foo'));
