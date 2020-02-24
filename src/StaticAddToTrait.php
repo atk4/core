777cab284ba36e9c7e2ade33f1cac1f7ea99fe53
Romans Malinovskis <me@nearly.guru>
2020-02-24 20:51:18 +0000

    Add POC for "CRUD::addTo($app)" syntax (#123)
    * Add POC for "CRUD::addTo($app)" syntax
    
    * Apply fixes from StyleCI
    
    * Add unit tests
    
    * Apply fixes from StyleCI
    
    * remove this weird file

---
+++ src/StaticAddToTrait.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace atk4\core;
+
+/**
+ * Trait StaticAddToTrait.
+ */
+trait StaticAddToTrait
+{
+    /**
+     * Check this property to see if trait is present in the object.
+     *
+     * @var bool
+     */
+    public $_staticAddToTrait = true;
+
+    /**
+     * A better way to initialize and add new object into parent - more typehinting-friendly.
+     *
+     * $crud = CRUD::addTo($app, ['displayFields'=>['name']]);
+     *
+     * is equivalent to
+     *
+     *
+     * //
+     *
+     * @param object $parent
+     * @param array  $seed
+     *
+     * @return self
+     */
+    public static function addTo(object $parent, $seed = [], ...$add_arguments)
+    {
+        if (isset($parent->_factoryTrait)) {
+            $object = $parent->factory(static::class, $seed);
+        } else {
+            $object = new static($seed);
+        }
+        $parent->add($object, ...$add_arguments);
+
+        return $object;
+    }
+}
