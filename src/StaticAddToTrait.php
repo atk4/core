4e1c388429dbb9ab274d3bcaf1a98f9983cfba7f
Michael Voříšek <mvorisek@mvorisek.cz>
2020-03-14 20:54:55 +0100

    Improve addTo method

--- src/StaticAddToTrait.php
+++ src/StaticAddToTrait.php
@@ -22,25 +22,39 @@ trait StaticAddToTrait
      * $crud = $app->add(['CRUD', 'displayFields' => ['name']]);
      *   but the first one design pattern is strongly recommended as it supports refactoring.
      *
-     * @param object       $parent
      * @param array|string $seed
      *
      * @return static
      */
     public static function addTo(object $parent, $seed = [], array $add_arguments = [])
     {
-        if (!is_array($seed) && !is_scalar($seed)) { // allow seed other than array but prevent bad usage
-            throw (new Exception(['Seed must be an array or a scalar']))
-                    ->addMoreInfo('seed_type', gettype($seed));
+        if (is_object($seed)) {
+            $object = $seed;
+        } else {
+            if (!is_array($seed)) {
+                if (!is_scalar($seed)) { // allow single element seed but prevent bad usage
+                    throw (new Exception(['Seed must be an array or a scalar']))
+                            ->addMoreInfo('seed_type', gettype($seed));
+                }
+
+                $seed = [$seed];
+            }
+
+            if (isset($parent->_factoryTrait)) {
+                $object = $parent->factory(static::class, $seed);
+            } else {
+                $object = new static(...$seed);
+            }
         }
 
-        if (isset($parent->_factoryTrait)) {
-            $object = $parent->factory(static::class, $seed);
-        } elseif (is_array($seed)) {
-            $object = new static(...$seed);
-        } else {
-            $object = new static($seed);
+        // check if object is instance of this class
+        if (!($object instanceof static)) {
+            throw (new Exception(['Seed class name is not a subtype of the current class']))
+                    ->addMoreInfo('seed_class', get_class($object))
+                    ->addMoreInfo('current_class', static::class);
         }
+
+        // add to parent
         $parent->add($object, ...$add_arguments);
 
         return $object;
