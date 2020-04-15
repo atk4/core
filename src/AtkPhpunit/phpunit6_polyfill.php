212da48ec3e955b0a0e75a9d16c81bf6fe22ccce
Michael Voříšek <mvorisek@mvorisek.cz>
2020-04-15 19:30:14 +0200

    Fix Cannot mix bracketed/unbracketed namespace declarations

--- src/AtkPhpunit/phpunit6_polyfill.php
+++ src/AtkPhpunit/phpunit6_polyfill.php
@@ -1,13 +1,12 @@
 <?php
 
-namespace atk4\core\AtkPhpunit;
-
-/**
- * Polyfill for phpunit < 7.0 to map commonly used classes to equivalent phpunit 7.0 namespaced names.
- */
-
-if (class_exists(\PHPUnit\Framework\TestCase::class)) { // do nothing for phpunit 7.0 or higher
-    return;
+namespace atk4\core\AtkPhpunit {
+    /**
+     * Polyfill for phpunit < 7.0 to map commonly used classes to equivalent phpunit 7.0 namespaced names.
+     */
+    if (class_exists(\PHPUnit\Framework\TestCase::class)) { // do nothing for phpunit 7.0 or higher
+        return;
+    }
 }
 
 namespace PHPUnit\Framework {
