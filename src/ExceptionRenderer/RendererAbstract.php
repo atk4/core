0adb948f5efe7ebf19086ab85b0142f6a2dbbdf0
Michael Voříšek <mvorisek@mvorisek.cz>
2020-06-07 19:17:50 +0200

    Fix exception renderer makeRelativePath() for empty path (#229)

--- src/ExceptionRenderer/RendererAbstract.php
+++ src/ExceptionRenderer/RendererAbstract.php
@@ -178,8 +178,7 @@ abstract class RendererAbstract
 
     protected function makeRelativePath(string $path): string
     {
-        $pathReal = realpath($path);
-        if ($pathReal === false) {
+        if ($path === '' || ($pathReal = realpath($path)) === false) {
             throw new Exception('Path not found');
         }
 
