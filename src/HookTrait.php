898b2b9ee26499dbd7e1a1ea592945f8f9fb3248
Michael Voříšek <mvorisek@mvorisek.cz>
2020-09-18 22:00:03 +0200

    add legit example

--- src/HookTrait.php
+++ src/HookTrait.php
@@ -55,6 +55,7 @@ trait HookTrait
                         // TODO we throw only if the class name is the same, otherwise the check is too strict
                         // and on a bad side - we should not throw when an object with a hook is cloned,
                         // but instead we should throw once the closure this object is cloned
+                        // example of legit use: https://github.com/atk4/audit/blob/eb9810e085a40caedb435044d7318f4d8dd93e11/src/Controller.php#L85
                         if (get_class($fxThis) === get_class($this->_hookOrigThis) || preg_match('~^atk4\\\\(?:core|dsql|data)~', get_class($fxThis))) {
                             throw (new Exception('Object can not be cloned with hook bound to a different object than this'))
                                 ->addMoreInfo('closure_file', $fxRefl->getFileName())
