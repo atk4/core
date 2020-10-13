f21b9cd682af4dbd7aeabfe6c1ce513f1a7da14f
DarkSide <imants.horsts@inbox.lv>
2020-10-14 00:39:26 +0300

    fix test on all php versions

--- tests/ContainerTraitTest.php
+++ tests/ContainerTraitTest.php
@@ -173,7 +173,7 @@ class ContainerTraitTest extends AtkPhpunit\TestCase
     public function testException3()
     {
         $this->expectException(\Error::class);
-        $this->expectExceptionMessage('Class "hello" not found');
+        $this->expectExceptionMessage('not found');
         $m = new ContainerMock();
         $m->add(['hello'], 123);
     }
