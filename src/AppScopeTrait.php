c992dfdaa3ca2b1ed8fecf8ae69b702d14ab1cfa
Francesco Danti <fdanti@gmail.com>
2020-12-28 17:01:37 +0100

    Removed method assertNoDirectAssignement. Already called inside method `issetApp` in first condition

--- src/AppScopeTrait.php
+++ src/AppScopeTrait.php
@@ -97,7 +97,6 @@ trait AppScopeTrait
      */
     public function setApp(object $app)
     {
-        $this->assertNoDirectAppAssignment();
         if ($this->issetApp() && $this->getApp() !== $app && $this->getApp() instanceof \Atk4\Ui\App) {
             if ($this->getApp()->catch_exceptions || $this->getApp()->always_run) { // allow to replace App created by AbstractView::initDefaultApp() - TODO fix
                 throw new Exception('App can not be replaced');
