44cb8a230680f1ba6e1e51a4f1b1c10480c9b37a
Michael Voříšek <mvorisek@mvorisek.cz>
2021-04-05 15:31:55 +0200

    add nested trait test

--- tests/TraitUtilTest.php
+++ tests/TraitUtilTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Atk4\Core\tests;
 
 use Atk4\Core\AtkPhpunit;
+use Atk4\Core\HookTrait;
 use Atk4\Core\NameTrait;
 use Atk4\Core\TraitUtil;
 
@@ -29,6 +30,10 @@ class TraitUtilTest extends AtkPhpunit\TestCase
         }, NameTrait::class));
         $this->assertTrue(TraitUtil::hasTrait(new class() extends TraitUtilTestC {
         }, NameTrait::class));
+
+        $this->assertFalse(TraitUtil::hasTrait(TraitUtilTestA::class, HookTrait::class));
+        $this->assertTrue(TraitUtil::hasTrait(TraitUtilTestB::class, HookTrait::class));
+        $this->assertTrue(TraitUtil::hasTrait(TraitUtilTestC::class, HookTrait::class));
     }
 }
 
@@ -36,9 +41,15 @@ class TraitUtilTestA
 {
 }
 
+trait TraitUtilTestTrait
+{
+    use HookTrait;
+}
+
 class TraitUtilTestB extends TraitUtilTestA
 {
     use NameTrait;
+    use TraitUtilTestTrait;
 }
 
 class TraitUtilTestC extends TraitUtilTestB
