ed76d1c6403be0b3edf7cff68839ef4ab6c433b5
Michael Voříšek <mvorisek@mvorisek.cz>
2022-07-06 16:36:02 +0200

    test coverage helper

---
+++ tests/Phpunit/TestCaseTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Atk4\Core\Tests\Phpunit;
+
+use Atk4\Core\Phpunit\TestCase;
+
+class TestCaseTest extends TestCase
+{
+    /** @var int */
+    private static $providerCallCounter = 0;
+
+    private function coverCoverageFromProvider(): void
+    {
+        self::$providerCallCounter++;
+    }
+
+    /**
+     * @dataProvider provideProviderCoverage
+     */
+    public function testProviderCoverage(string $v): void
+    {
+        $this->assertGreaterThan(1, self::$providerCallCounter);
+        $this->assertTrue(in_array($v, ['x', 'y'], true));
+    }
+
+    public function provideProviderCoverage(): \Traversable
+    {
+        yield ['x'];
+        $this->coverCoverageFromProvider();
+        yield ['y'];
+    }
+}
