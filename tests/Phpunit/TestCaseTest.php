041e56b113b873904f3cb50c7b2313814d3af9cc
Michael Voříšek <mvorisek@mvorisek.cz>
2022-07-07 23:00:14 +0200

    test fix for incomplete

--- tests/Phpunit/TestCaseTest.php
+++ tests/Phpunit/TestCaseTest.php
@@ -24,6 +24,8 @@ class TestCaseTest extends TestCase
     {
         if ($v === 'y') {
             $this->assertSame(2, self::$providerCallCounter);
+        } else {
+            $this->markTestIncomplete('test');
         }
         $this->assertTrue(in_array($v, ['a', 'x', 'y'], true));
     }
