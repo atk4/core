9334dab98ae175977f2eb0ec4104d1d3be5830ae
Michael Voříšek <mvorisek@mvorisek.cz>
2022-07-08 01:12:30 +0200

    cover phpunit ResultPrinter

---
+++ tests/Phpunit/ResultPrinterTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Atk4\Core\Tests\Phpunit;
+
+use Atk4\Core\Exception;
+use Atk4\Core\Phpunit\ResultPrinter;
+use Atk4\Core\Phpunit\TestCase;
+use PHPUnit\Framework\ExceptionWrapper;
+use PHPUnit\Framework\TestFailure;
+
+class ResultPrinterTest extends TestCase
+{
+    /**
+     * @param class-string<ResultPrinter> $resultPrinterClass
+     */
+    private function printAndReturnDefectTrace($resultPrinterClass, \Throwable $exception): string
+    {
+        $defect = new TestFailure($this, $exception);
+        $stream = fopen('php://memory', 'w+');
+        $printer = new $resultPrinterClass($stream);
+        \Closure::bind(fn () => $printer->printDefectTrace($defect), null, $resultPrinterClass)();
+        fseek($stream, 0);
+
+        return stream_get_contents($stream);
+    }
+
+    public function testBasic(): void
+    {
+        $innerException = new \Error('Inner Exception');
+        $exception = (new Exception('My exception', 0, $innerException))
+            ->addMoreInfo('x', 'foo')
+            ->addMoreInfo('y', ['bar' => 2.4, [], [[1]]]);
+
+        $resNotWrapped = $this->printAndReturnDefectTrace(ResultPrinter::class, $exception);
+        $this->assertStringContainsString((string) $exception, $resNotWrapped);
+        $this->assertStringContainsString((string) $innerException, $resNotWrapped);
+
+        $res = $this->printAndReturnDefectTrace(ResultPrinter::class, new ExceptionWrapper($exception));
+        $this->assertTrue(strlen($res) < strlen($resNotWrapped));
+        if (\PHP_MAJOR_VERSION < 8) {
+            // phpvfscomposer:// is not correctly filtered from stacktrace
+            // by PHPUnit\Util\Filter::getFilteredStacktrace() method
+            return;
+        }
+        $this->assertSame(
+            <<<'EOF'
+                Atk4\Core\Exception: My exception
+                  x: 'foo'
+                  y: [
+                      'bar': 2.4,
+                      0: [],
+                      1: [
+                          ...
+                        ]
+                    ]
+
+                self.php:36
+
+                Caused by
+                Error: Inner Exception
+
+                self.php:35
+                EOF . "\n", // NL in the string is not parsed by Netbeans, see https://github.com/apache/netbeans/issues/4345
+            str_replace(__FILE__, 'self.php', $res)
+        );
+    }
+}
