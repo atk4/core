bb067ffc299858fd3d83580b8a557aef188cf570
Michael Voříšek <mvorisek@mvorisek.cz>
2022-07-08 02:07:04 +0200

    test no assertion coverage fix impl

--- tests/Phpunit/TestCaseTest.php
+++ tests/Phpunit/TestCaseTest.php
@@ -22,17 +22,18 @@ class TestCaseTest extends TestCase
      * @dataProvider provideProviderCoverage1
      * @dataProvider provideProviderCoverage2
      */
-    public function testProviderCoverage1(string $v): void
+    public function testProviderCoverage(string $v): void
     {
         if ($v === 'y') {
             $this->assertSame(2, self::$providerCallCounter);
         }
-        $this->assertTrue(in_array($v, ['a', 'x', 'y'], true));
+        $this->assertTrue(in_array($v, ['a', 'b', 'x', 'y'], true));
     }
 
     public function provideProviderCoverage1(): \Traversable
     {
         yield ['a'];
+        yield ['b'];
     }
 
     public function provideProviderCoverage2(): \Traversable
@@ -42,6 +43,38 @@ class TestCaseTest extends TestCase
         yield ['y'];
     }
 
+    /**
+     * @dataProvider provideProviderCoverage1
+     */
+    public function testCoverageImplForDoesNotPerformAssertions(string $v): void
+    {
+        $this->assertFalse($this->doesNotPerformAssertions());
+        $this->assertTrue($this->getTestResultObject()->isStrictAboutTestsThatDoNotTestAnything());
+
+        if ($v === 'b') {
+            // make sure TestResult::$beStrictAboutTestsThatDoNotTestAnything is reset
+            // after this test using phpunit AfterTestHook
+            return;
+        }
+
+        $testStatusOrig = \Closure::bind(fn () => $this->status, $this, PhpunitTestCase::class)();
+        \Closure::bind(fn () => $this->status = BaseTestRunner::STATUS_PASSED, $this, PhpunitTestCase::class)();
+        try {
+            \Closure::bind(fn () => $this->doesNotPerformAssertions = true, $this, PhpunitTestCase::class)();
+            try {
+                $this->tearDown();
+            } finally {
+                \Closure::bind(fn () => $this->doesNotPerformAssertions = false, $this, PhpunitTestCase::class)();
+            }
+        } finally {
+            \Closure::bind(fn () => $this->status = $testStatusOrig, $this, PhpunitTestCase::class)();
+        }
+        $this->assertFalse($this->getTestResultObject()->isStrictAboutTestsThatDoNotTestAnything());
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
     public function testCoverageImplForTestMarkedAsIncomplete(): void
     {
         $testStatusOrig = \Closure::bind(fn () => $this->status, $this, PhpunitTestCase::class)();
