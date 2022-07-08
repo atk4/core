e42978388f859d8bf8fec1cc2960b904242fdae2
Michael Voříšek <mvorisek@mvorisek.cz>
2022-07-08 04:20:25 +0200

    test WithWarnDynamicPropertyTrait trait

---
+++ tests/WarnDynamicPropertyTraitTest.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Atk4\Core\Tests;
+
+use Atk4\Core\Exception as ClassWithWarnDynamicPropertyTrait;
+use Atk4\Core\Phpunit\TestCase;
+
+class WarnDynamicPropertyTraitTest extends TestCase
+{
+    protected function runWithErrorConvertedToException(\Closure $fx): void
+    {
+        set_error_handler(function (int $errno, string $errstr): void {
+            throw new WarnError($errstr);
+        });
+        try {
+            $fx();
+        } finally {
+            restore_error_handler();
+        }
+    }
+
+    public function testIssetException(): void
+    {
+        $this->runWithErrorConvertedToException(function () {
+            $test = new ClassWithWarnDynamicPropertyTrait();
+
+            $this->expectException(WarnError::class);
+            $this->expectErrorMessage('Undefined property: Atk4\Core\Exception::$xxx');
+            isset($test->xxx);
+        });
+    }
+
+    public function testGetException(): void
+    {
+        $this->runWithErrorConvertedToException(function () {
+            $test = new ClassWithWarnDynamicPropertyTrait();
+
+            $this->expectException(WarnError::class);
+            $this->expectErrorMessage('Undefined property: Atk4\Core\Exception::$xxx');
+            $test->xxx;
+        });
+    }
+
+    public function testSetException(): void
+    {
+        $this->runWithErrorConvertedToException(function () {
+            $test = new ClassWithWarnDynamicPropertyTrait();
+
+            $this->expectException(WarnError::class);
+            $this->expectErrorMessage('Undefined property: Atk4\Core\Exception::$xxx');
+            $test->xxx = 5;
+        });
+    }
+
+    public function testUnsetException(): void
+    {
+        $this->runWithErrorConvertedToException(function () {
+            $test = new ClassWithWarnDynamicPropertyTrait();
+
+            $this->expectException(WarnError::class);
+            $this->expectErrorMessage('Undefined property: Atk4\Core\Exception::$xxx');
+            unset($test->xxx);
+        });
+    }
+
+    public function testGetProtectedPropertyException(): void
+    {
+        $test = new ClassWithWarnDynamicPropertyTrait();
+
+        $this->expectException(\Error::class);
+        $this->expectErrorMessage('Cannot access protected property Atk4\Core\Exception::$customExceptionTitle');
+        $test->customExceptionTitle;
+    }
+}
+
+class WarnError extends \Exception
+{
+}
