9fff07afca3070d6f0ec6004c5f8974b8ab698d4
Michael Voříšek <mvorisek@mvorisek.cz>
2024-04-02 21:20:21 +0200

    fake phpstan trait interfaces/assertions

--- src/TraitUtil.php
+++ src/TraitUtil.php
@@ -58,11 +58,20 @@ final class TraitUtil
     // StaticAddToTrait - not used
     // TranslatableTrait - not used
 
+    /**
+     * @phpstan-assert-if-true TraitUtilAppScopeTrait $obj
+     * @phpstan-assert-if-true int $obj->maxNameLength
+     * @phpstan-assert-if-true array<string, string> $obj->uniqueNameHashes
+     */
     public static function hasAppScopeTrait(object $obj): bool
     {
         return self::hasTrait($obj, AppScopeTrait::class);
     }
 
+    /**
+     * @phpstan-assert-if-true TraitUtilContainerTrait $obj
+     * @phpstan-assert-if-true array<string, object> $obj->elements
+     */
     public static function hasContainerTrait(object $obj): bool
     {
         return self::hasTrait($obj, ContainerTrait::class);
@@ -70,6 +79,8 @@ final class TraitUtil
 
     /**
      * Used in Factory and in ui/View only.
+     *
+     * @phpstan-assert-if-true TraitUtilDiContainerTrait $obj
      */
     public static function hasDiContainerTrait(object $obj): bool
     {
@@ -78,22 +89,36 @@ final class TraitUtil
 
     /**
      * Used in DynamicMethodTrait only.
+     *
+     * @phpstan-assert-if-true TraitUtilHookTrait $obj
+     * @phpstan-assert-if-true array<string, array<int, array<int, array{\Closure, 1?: array<int, mixed>}>>> $obj->hooks
      */
     public static function hasHookTrait(object $obj): bool
     {
         return self::hasTrait($obj, HookTrait::class);
     }
 
+    /**
+     * @phpstan-assert-if-true TraitUtilInitializerTrait $obj
+     */
     public static function hasInitializerTrait(object $obj): bool
     {
         return self::hasTrait($obj, InitializerTrait::class);
     }
 
+    /**
+     * @phpstan-assert-if-true TraitUtilNameTrait $obj
+     * @phpstan-assert-if-true string $obj->name
+     */
     public static function hasNameTrait(object $obj): bool
     {
         return self::hasTrait($obj, NameTrait::class);
     }
 
+    /**
+     * @phpstan-assert-if-true TraitUtilTrackableTrait $obj
+     * @phpstan-assert-if-true string $obj->shortName
+     */
     public static function hasTrackableTrait(object $obj): bool
     {
         return self::hasTrait($obj, TrackableTrait::class);
