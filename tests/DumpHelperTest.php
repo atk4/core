4c4c3eba32362167a6fb5d88d73e0f4486304d3b
Michael Voříšek <mvorisek@mvorisek.cz>
2025-05-19 16:00:40 +0200

    Revert "hotfix CI" to reproduce the issue

--- tests/DumpHelperTest.php
+++ tests/DumpHelperTest.php
@@ -122,8 +122,6 @@ class DumpHelperTest extends TestCase
     {
         [$value, $expectedDuplicateOids, $expectedDuplicateRids] = $makeCaseFx();
 
-        self::getRid($value); // TODO remove asap, hotfix CI PHP 8.2+ with coverage
-
         $dumpHelper = new DumpHelper();
         $duplicateOids = [];
         $duplicateRids = [];
